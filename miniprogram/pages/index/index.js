9b64abd77320df57f95db1f276ebdf5d6ea90568
unknown <1339531047@qq.com>
2021-08-11 16:59:04 +0800

    feat: 新增小金库、收入与支出对比图表、可支配金额图表、收入支出详细信息头部

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,1 +1,11 @@
-Page({})
+Page({
+  data: {
+    totalMoney: '100000.00',
+    monthCanUseMoney: '2000.00',
+    monthLeftoverMoney: '1500.00',
+    dayCanUseMoney: '40.00',
+    dayLeftoverMoney: '23.00',
+    income: '80000.00',
+    expend: '20000.00'
+  }
+})
