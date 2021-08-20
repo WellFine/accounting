97d57c72f1b7753d9da7483dd840225cbd57c866
unknown <1339531047@qq.com>
2021-08-20 17:49:28 +0800

    feat: 新增加载动画组件

---
+++ miniprogram/components/common/loading/loading.js
@@ -0,0 +1,20 @@
+Component({
+  properties: {
+    type: {
+      type: String,
+      value: 'line'
+    },
+    width: {
+      type: String,
+      value: '100'
+    },
+    height: {
+      type: String,
+      value: '100'
+    },
+    color: {
+      type: String,
+      value: '#40a9ff'
+    }
+  }
+})
