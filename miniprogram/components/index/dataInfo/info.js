e7b826894d291af2701c59fb34e05d590adb4463
unknown <1339531047@qq.com>
2021-08-20 17:50:46 +0800

    feat: 新增详细信息和加载动画

--- miniprogram/components/index/dataInfo/info.js
+++ miniprogram/components/index/dataInfo/info.js
@@ -1,2 +1,35 @@
+import { getEndTime } from '../../../utils/date'
+import { request } from '../../../utils/request'
+
 Component({
+  data: {
+    list: [],
+    isLoading: true
+  },
+  methods: {
+    details (e) {
+      this.setData({
+        isLoading: true
+      })
+      
+      const { type, name, date } = e.detail
+
+      // 获取本月时间戳
+      const beginTime = Date.parse(date)
+      const endTime = getEndTime(date)
+      
+      request('getDetailsByTimeGroup', {
+        type,
+        name,
+        beginTime,
+        endTime
+      }).then(res => {
+        const { list } = res.result
+        this.setData({
+          list,
+          isLoading: false
+        })
+      })
+    }
+  }
 })
