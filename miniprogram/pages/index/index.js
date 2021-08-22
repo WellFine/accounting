83d9e6e51920a6f8807ccc9803bee0ccf5520167
unknown <1339531047@qq.com>
2021-08-23 00:38:42 +0800

    refactor: 删除每日可支配组件; feat: 兼容加载失败场景

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,11 +1,82 @@
+import { request } from '../../utils/request'
+import { getTimestamp } from '../../utils/date'
+import { padMoney } from '../../utils/money'
+import { subtraction, toast } from '../../utils/util'
+import { getAccountBalance, getMonthCanUseMoney } from '../../utils/storage'
+
 Page({
   data: {
-    totalMoney: '100000.00',
-    monthCanUseMoney: '2000.00',
-    monthLeftoverMoney: '1500.00',
-    dayCanUseMoney: '40.00',
-    dayLeftoverMoney: '23.00',
-    income: '80000.00',
-    expend: '20000.00'
+    totalMoney: '',
+    monthCanUseMoney: '',
+    monthLeftoverMoney: '',
+    expend: '',
+    income: '',
+
+    isNetworkError: false,
+    isReloading: false
+  },
+
+  onLoad () {
+    // 从缓存中去除账户余额以及每月可支配金额
+    this.setData({
+      totalMoney: padMoney(getAccountBalance()),
+      monthCanUseMoney: padMoney(getMonthCanUseMoney())
+    })
+    
+    // 加载每月总收入与总支出以及月剩余可支配金额
+    this._load()
+  },
+
+  // 重新加载数据
+  reload () {
+    this.setData({
+      isReloading: true
+    })
+
+    this._load()
+  },
+
+  /**
+   * 用于加载每月总收入与总支出以及月剩余可支配金额
+   */
+  _load () {
+    const { beginTime, endTime } = getTimestamp()
+
+    // 获取每月总收入与总支出
+    request('getDetailsByTypeGroup', {
+      beginTime,
+      endTime
+    }).then(res => {
+      const { list } = res.result,
+            expend = padMoney(list[0].money * 100 / 100),
+            income = padMoney(list[1].money * 100 / 100)
+      
+      // 加载月剩余可支配金额
+      this._loadMonthLeftoverMoney(expend)
+            
+      this.setData({
+        expend,
+        income,
+        isNetworkError: false
+      })
+    }).catch(() => {
+      toast('收入与支出加载失败')
+      this.setData({
+        isNetworkError: true
+      })
+    }).finally(() => {
+      this.setData({
+        isReloading: false
+      })
+    })
+  },
+
+  _loadMonthLeftoverMoney (expend) {
+    const { monthCanUseMoney } = this.data
+    let monthLeftoverMoney = subtraction(monthCanUseMoney, expend)
+
+    this.setData({
+      monthLeftoverMoney: padMoney(monthLeftoverMoney)
+    })
   }
 })
