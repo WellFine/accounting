215760224cae00b7cff08cb376e52838ba05e8a4
unknown <1339531047@qq.com>
2021-08-22 16:39:39 +0800

    feat: 兼容网络错误场景

--- miniprogram/components/index/dataInfo/info.js
+++ miniprogram/components/index/dataInfo/info.js
@@ -1,23 +1,46 @@
 import { getEndTime } from '../../../utils/date'
 import { request } from '../../../utils/request'
+import { toast } from '../../../utils/util'
 
 Component({
   data: {
     list: [],
-    isLoading: true
+    isLoading: true,
+    type: 0,
+    name: '',
+    beginTime: '',
+    endTime: '',
+    isNetworkError: false
   },
   methods: {
     details (e) {
-      this.setData({
-        isLoading: true
-      })
-      
       const { type, name, date } = e.detail
-
+      
       // 获取本月时间戳
       const beginTime = Date.parse(date)
       const endTime = getEndTime(date)
       
+      this.setData({
+        type,
+        name,
+        beginTime,
+        endTime
+      })
+
+      this._load()
+    },
+
+    reload () {
+      this._load()
+    },
+
+    _load () {
+      this.setData({
+        isLoading: true
+      })
+      
+      const { type, name, beginTime, endTime } = this.data
+
       request('getDetailsByTimeGroup', {
         type,
         name,
@@ -27,6 +50,15 @@ Component({
         const { list } = res.result
         this.setData({
           list,
+          isNetworkError: false
+        })
+      }).catch(() => {
+        toast('网络去出差啦，稍后再试吧')
+        this.setData({
+          isNetworkError: true
+        })
+      }).finally(() => {
+        this.setData({
           isLoading: false
         })
       })
