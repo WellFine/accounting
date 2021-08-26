a3f6ce9773ec0afc26b0d6d5ae2235625c5ce026
unknown <1339531047@qq.com>
2021-08-26 21:33:54 +0800

    feat: 新增点击扇形加载该类型下的子项图表

--- miniprogram/components/details/pie/pie.js
+++ miniprogram/components/details/pie/pie.js
@@ -1,7 +1,7 @@
 import * as echarts from '../../ec-canvas/echarts'
 
 import { request } from '../../../utils/request'
-import { getEndTime } from '../../../utils/date'
+import { getEndTime, getTimestamp } from '../../../utils/date'
 
 Component({
   properties: {
@@ -14,7 +14,14 @@ Component({
       lazyLoad: true
     },
     isLoading: true,
-    data: []
+    data: [],
+
+    ecSub: {
+      lazyLoad: true
+    },
+    isLoadSub: false,     // 控制 sub 块的出现与否
+    isLoadingSub: false,  // 控制 sub 块的加载动画
+    subData: []
   },
 
   observers: {
@@ -22,7 +29,8 @@ Component({
       if (!date || type < 0 || type > 2) return
 
       this.setData({
-        isLoading: true
+        isLoading: true,    // 开启类型加载动画
+        isLoadSub: false    // 关闭类型子项图表
       })
 
       request('getDetailsByNameGroup', {
@@ -107,6 +115,119 @@ Component({
             }
           },
           data: data.map(item => {
+            const percent = ((item.money / total.money) * 100).toFixed(2)
+            return {
+              value: item.money,
+              name: `${item._id} ${percent}%`
+            }
+          }),
+        }]
+      }
+
+      chart.setOption(option)
+
+      chart.on('click', this._loadSubChart.bind(this))
+    },
+
+    _loadSubChart (params) {
+      const { name } = params
+      const mainName = name.split(' ')[0]
+      const { date, type } = this.properties
+      const { beginTime, endTime } = getTimestamp(new Date(date))
+      
+      this.setData({
+        isLoadSub: true,
+        isLoadingSub: true
+      })
+
+      request('getDetailsBySubGroup', {
+        type,
+        name: mainName,
+        beginTime,
+        endTime
+      }).then(res => {
+        /**
+         * list 的每一项
+         * _id：具体的 sub 名称
+         * money：改组总金额
+         */
+        const { list } = res.result
+
+        for (const item of list) {
+          item.money /= 100
+          if (!item._id) {    // 有可能该类型没有 sub 子项，那么直接设为类型名称即可
+            item._id = mainName
+          }
+        }
+        
+        this.setData({
+          subData: list,
+          isLoadingSub: false
+        })
+
+        this.ecSubComponent = this.selectComponent('#sub-pie')
+        this._initSubChart()
+      })
+    },
+
+    _initSubChart () {
+      this.ecSubComponent.init((canvas, width, height, dpr) => {
+        // 初始化图表
+        const chart = echarts.init(canvas, null, {
+          width,
+          height,
+          devicePixelRatio: dpr
+        })
+
+        this._setSubOption(chart)
+        
+        // 返回 chart 实例，否则会影响事件处理等
+        return chart
+      })
+    },
+
+    _setSubOption (chart) {
+      const { subData } = this.data
+
+      const total = subData.reduce((prev, cur) => {
+        return {
+          money: prev.money + cur.money
+        }
+      })
+
+      const option = {
+        legend: {
+          type: 'scroll'
+        },
+        tooltip: {
+          show: true
+        },
+        series: [{
+          type: 'pie',
+          radius: ['25%', '70%'],
+          center: ['50%', '55%'],
+          itemStyle: {  // 设置扇形样式
+            borderRadius: 4,
+            borderColor: '#fff',
+            borderWidth: 2
+          },
+          label: {      // 设置文字
+            show: true,
+            position: 'outer',
+            alignTo: 'labelLine',   // 开启文字对齐
+            bleedMargin: 0,
+          },
+          labelLine: {  // 设置指导线
+            show: true,
+            smooth: true, // 设置第一段指导线平滑过渡到第二段指导线
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontWeight: 'bold'
+            }
+          },
+          data: subData.map(item => {
             const percent = ((item.money / total.money) * 100).toFixed(2)
             return {
               value: item.money,
